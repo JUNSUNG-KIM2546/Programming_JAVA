2a5921e4b996250397e7f6903ae62934a7b04007
Student <Student@Student-PC>
2023-04-24 21:59:04 +0900

    04.24

--- polytech_Java/src/game/GameUnit.java
+++ polytech_Java/src/game/GameUnit.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.util.Scanner;
 
 public class GameUnit {
 
@@ -7,9 +8,8 @@ public class GameUnit {
 	private int unitPower = 5000;
 	private MyWeapon weapon;
 	private String name;
+	static int coin;
 	
-	
-	//private Scanner name = new Scanner(System.in);
 	
 	//생성자를 구현해주세요.
 	public GameUnit(String name) {
@@ -24,34 +24,110 @@ public class GameUnit {
 		GameUnit.bossPower = bossPower - power;
 	}
 	
+	void Coin(int coin) {
+		
+		this.coin = coin;
+	}
+	
 	public static void main(String[] args) {
 		
-		GameUnit unit1 = new GameUnit("unit1");
-		GameUnit unit2 = new GameUnit("unit2");
+		Scanner sc = new Scanner(System.in);
 		
-
+		GameUnit unit1 = new GameUnit("데마시아");
+		GameUnit unit2 = new GameUnit("녹서스");
+		coin = 1;
 		
-		while(true) {
+		//Coin co = new Coin(coin);
 		
-		
+		while(0<coin) {
+			System.out.println("원하는 진영을 선택 해주세요.(데마시아 : 1 / 녹서스 : 2 ) : ");
+			String a = sc.next();
 			
-			unit1.attack((int)(1+Math.random() * 500));
-			System.out.println(unit1);
+			switch (a) {
+			case "1":
+				System.out.println("보유 코인 : " + coin);
+				System.out.println("코인을 원하는 만큼 배팅 해주세요.");
+				int co = sc.nextInt();
+				coin=(coin-co);
 				
-	
-			unit2.attack((int)(1+Math.random() * 500));
-			System.out.println(unit2);
+				while(true) {
+					
+					unit1.attack((int)(1+Math.random() * 500));
+					System.out.println(unit1.name + " 공격!");
+					
+					if(GameUnit.bossPower <= 0) {
+						System.out.println("< " + unit1.name + " 승리! >");
+						System.out.println("< 게임을 종료 합니다. >");
+						coin=(coin+(co*2));
+						break;
+						//System.exit(0);
+					}
+					
+					else
+						System.out.println("' 킹왕짱 빌런 ' 남은 HP : " + GameUnit.bossPower);
 			
-			if(GameUnit.bossPower <= 0) {
-				System.out.println("승리! - 게임을 종료합니다.");
-				System.exit(0);
-			}
-			else
-				System.out.println("남은 bossPower : " + GameUnit.bossPower);
-			
-			
-		}
+					
+					unit2.attack((int)(1+Math.random() * 500));
+					System.out.println(unit2.name + " 공격! ");
+					
+					if(GameUnit.bossPower <= 0) {
+						System.out.println("< " + unit2.name + " 승리! >");
+						System.out.println("< 게임을 종료 합니다. >");
+						break;
+						//System.exit(0);
+					}
+					
+					else
+						System.out.println("' 킹왕짱 빌런 ' 남은 HP : " + GameUnit.bossPower);
+					
+				}
+				
+				//break;
 
+			case "2":
+				System.out.println("보유 코인 : " + coin);
+				System.out.println("코인을 원하는 만큼 배팅 해주세요.");
+				int co2 = sc.nextInt();
+				coin=(coin-co2);
+				
+				while(true) {
+					
+					unit1.attack((int)(1+Math.random() * 500));
+					System.out.println(unit1.name + " 공격!");
+					
+					if(GameUnit.bossPower <= 0) {
+						System.out.println("< " + unit1.name + " 승리! >");
+						System.out.println("< 게임을 종료 합니다. >");
+						break;
+						//System.exit(0);
+					}
+					
+					else
+						System.out.println("' 킹왕짱 빌런 ' 남은 HP : " + GameUnit.bossPower);
+			
+					
+					unit2.attack((int)(1+Math.random() * 500));
+					System.out.println(unit2.name + " 공격! ");
+					
+					if(GameUnit.bossPower <= 0) {
+						System.out.println("< " + unit2.name + " 승리! >");
+						System.out.println("< 게임을 종료 합니다. >");
+						coin=(coin+(co2*2));
+						break;
+						//System.exit(0);
+					}
+					
+					else
+						System.out.println("' 킹왕짱 빌런 ' 남은 HP : " + GameUnit.bossPower);
+					
+				}
+				
+				//break;
+			}
+		
+					
+		}
+					
 	}
 	
 
