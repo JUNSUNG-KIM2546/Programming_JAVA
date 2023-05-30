e8cf4db1fe1621b8a2fcdbab7408dc26181297c4
Student <Student@Student-PC>
2023-05-30 16:03:32 +0900

    05.30

--- polytech_Java/src/miniproject_db/Product.java
+++ polytech_Java/src/miniproject_db/Product.java
@@ -22,6 +22,30 @@ public abstract class Product {
 //		return price;
 //	}
 
+	public int getPcode() {
+		return pcode;
+	}
+
+	public void setPcode(int pcode) {
+		this.pcode = pcode;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
 	public abstract void printExtra();
 	
 }
